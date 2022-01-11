6b564d0a1091f4d4e90d1b6e8838f458fc738b78
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-01-11 19:31:06 -0300

    Fix browser notification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { ChallengeBox } from "../components/ChallengeBox"
 import Head from 'next/head'
 
 import styles from "../styles/pages/Home.module.css"
+import { CountdownProvider } from "../contexts/CountdownContext"
 
 export default function Home() {
   return (
@@ -14,16 +15,20 @@ export default function Home() {
         <title>Início | move.it</title>
       </Head>
       <ExperienceBar />
-      <section>
-        <div>
-          <Profile />
-          <CompletedChanllenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChanllenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
+      
     </div>
   )
 }
